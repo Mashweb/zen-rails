656dbf68f6ba7c651ca6ce9ec72262790a9ac6ff
Tom Elam <tomelam@gmail.com>
2021-04-10 16:48:00 +0530

    Got the proxy controller to work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/web/:url" => 'proxy#open', :constraints => { :url => /[^\/]+/ }
+  #get "/web/:url" => 'proxy#open', :constraints => { :url => /[^\/]+/ }
+  #get "/web/:url" => 'proxy#open', :constraints => { :url => /[a-zA-Z0-9\.-]+/ }
+  #get "/web/:url" => 'proxy#open', :constraints => { :url => /[a-zA-Z0-9\.\:-]+/ }
+  #get "/web/:url" => 'proxy#open', :constraints => { :url => /[a-zA-Z0-9\.\:\/-]+/ }
+  get "/web/:url" => 'proxy#open', :constraints => { :url => /[a-zA-Z0-9\.\:\/\_-]+/ }
 end
